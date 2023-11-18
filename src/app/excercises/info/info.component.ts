25ac5d0d3a8b41d92a16cd48b5a6cc0df58eb2cc
AbrilGala <abrilbelengala@gmail.com>
2023-11-18 12:58:39 -0300

    Cambios en el front

--- src/app/excercises/info/info.component.ts
+++ src/app/excercises/info/info.component.ts
@@ -26,60 +26,66 @@ export class InfoComponent implements OnInit {
       const result: string = await response.text();
       console.log(result);
       this.uploadExcercises(result);
-      this.showExcercises();
+      // this.showExcercises();
     } catch (error) {
       console.error(error);
     }
   }
+
   uploadExcercises(result: string): void {
     try {
       let data = JSON.parse(result);
-    for(let i=0; i<result.length; i++) {
-      if(data[i]!=null){
-      const difficulty: string = data[i].difficulty;
-      const equipment: string = data[i].equipment;
-      const instructions: string = data[i].instructions;
-      const muscle: string = data[i].muscle;
-      const name: string = data[i].name;
-      const type: string = data[i].type;
-      const excercise = new Excercise(difficulty, equipment, instructions, muscle, name, type);
-      this.excerciseService.addExcercise(excercise);
-      this.excerciseList = this.excerciseService.getExcercises();
+      for (let i = 0; i < result.length; i++) {
+        if (data[i] != null) {
+          const difficulty: string = data[i].difficulty;
+          const equipment: string = data[i].equipment;
+          const instructions: string = data[i].instructions;
+          const muscle: string = data[i].muscle;
+          const name: string = data[i].name;
+          const type: string = data[i].type;
+          const exercise = new Excercise(difficulty, equipment, instructions, muscle, name, type);
+          this.excerciseService.addExcercise(exercise);
+          this.excerciseList = this.excerciseService.getExcercises();
+        }
       }
-    }
-    console.log(this.excerciseList);
     } catch (error) {
       console.log(error);
     }
   }
-  showExcercises(){
-    let container = document.getElementById("container");
-    let resultado = "";
-    for(let i=0; i<this.excerciseList.length; i++){
-      let midiv = document.createElement('div');
-      let miH1 = document.createElement('h1');
-      let pType = document.createElement('p');
-      let pDifficulty = document.createElement('p');
-      miH1.textContent = this.excerciseList[i].getName();
-      pType.textContent = this.excerciseList[i].getType();
-      pDifficulty.textContent = this.excerciseList[i].getDifficulty();
-      midiv.appendChild(miH1);
-      midiv.appendChild(pType);
-      midiv.appendChild(pDifficulty);
-      resultado += midiv.outerHTML;      
+  // showExcercises(){
+  //   let container = document.getElementById("container");
+  //   let resultado = "";
+  //   for(let i=0; i<this.excerciseList.length; i++){
+  //     let midiv = document.createElement('div');
+  //     let miH1 = document.createElement('h1');
+  //     let pType = document.createElement('p');
+  //     let pDifficulty = document.createElement('p');
+
+  //     midiv.classList.add('exercise-item');
+  //     miH1.classList.add('exercise-name');
+  //     pType.classList.add('exercise-type');
+  //     pDifficulty.classList.add('exercise-difficulty');
+
+  //     miH1.textContent = this.excerciseList[i].getName();
+  //     pType.textContent = this.excerciseList[i].getType();
+  //     pDifficulty.textContent = this.excerciseList[i].getDifficulty();
+  //     midiv.appendChild(miH1);
+  //     midiv.appendChild(pType);
+  //     midiv.appendChild(pDifficulty);
+  //     resultado += midiv.outerHTML;      
   
-    }
-    if (container != null) {
-      container.innerHTML = resultado;
-      // Ahora, después de agregar los midiv al contenedor, puedes agregar el event listener
-      for (let i = 0; i < this.excerciseList.length; i++) {
-        let midiv = container.children[i];
-        if (midiv) {
-          midiv.addEventListener('click', function () {
-            window.location.href = 'login';
-          });
-        }
-      }
-    }
-  }
+  //   }
+  //   if (container != null) {
+  //     container.innerHTML = resultado;
+  //     // Ahora, después de agregar los midiv al contenedor, puedes agregar el event listener
+  //     for (let i = 0; i < this.excerciseList.length; i++) {
+  //       let midiv = container.children[i];
+  //       if (midiv) {
+  //         midiv.addEventListener('click', function () {
+  //           window.location.href = 'login';
+  //         });
+  //       }
+  //     }
+  //   }
+  // }
 }
